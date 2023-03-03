9ef17c169c9254a6bc82bb043204f2ad46cabfcd
Selim Oğuz Şahin <selimoguz38@outlook.com>
2023-03-03 20:41:34 +0300

    Success

---
+++ BasicDataTypes/FindtheRunner_UpScore.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+arr = map(int, input().split())
+arr2 = sorted(arr, reverse=True)
+
+for i,x in enumerate(arr2):
+    if max(arr2)==arr2[i]:
+       pass
+    else:
+        print(x)
+        break
